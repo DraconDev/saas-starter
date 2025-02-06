0b893d568188398e7f4e8b475ccfb55173440b3c
DraconDev <dracsharp@gmail.com>
2025-02-06 22:32:24 +0000

    Update Stripe API version to 2024-06-20 in stripe.ts

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -8,7 +8,7 @@ import {
 } from "@/lib/db/queries";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-01-27.acacia",
 });
 
 export async function createCheckoutSession({
